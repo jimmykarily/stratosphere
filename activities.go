3be1f77fcc8d45701482e026c687067f4cd74e02
Dimitris Karakasilis <jimmykarily@gmail.com>
2019-11-11 20:17:16 +0200

    Show activities as vertical tabs

--- activities.go
+++ activities.go
@@ -1,44 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 
 	"fyne.io/fyne"
-	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
-	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
 
-// Callbacks must be closures or your callback will use the last value
-// of the activity variable in the for loop!
-// https://tour.golang.org/moretypes/25
-func loadActivityCallback(activity *Activity) func() {
-	return func() {
-		fmt.Println(activity.Content)
-		fmt.Println(activity.Id)
-	}
+func activityDetails(activity *Activity) *widget.Group {
+	generic := widget.NewGroupWithScroller("Values")
+	generic.Prepend(widget.NewLabel("Activity Id: " + strconv.Itoa(activity.Id)))
+
+	return generic
 }
 
 func makeActivitiesList(application App) fyne.Widget {
-	logo := canvas.NewImageFromResource(theme.FyneLogo())
-	logo.SetMinSize(fyne.NewSize(320, 320))
-	list := widget.NewVBox()
+	activityList := []*widget.TabItem{}
+
 	activities, err := application.db.Activities()
 	if err != nil {
 		dialog.ShowError(err, application.window)
 	}
 
 	for _, activity := range activities {
-		list.Append(widget.NewButton(activity.Content.StartTimeStr(), loadActivityCallback(activity)))
+		activityList = append(activityList,
+			widget.NewTabItemWithIcon(activity.Content.StartTimeStr(), nil, activityDetails(activity)),
+		)
 	}
 
-	scroll := widget.NewScrollContainer(list)
-	scroll.Resize(fyne.NewSize(200, 200))
-	container := widget.NewHBox(scroll)
+	activityTabs := widget.NewTabContainer(activityList...)
+	activityTabs.SetTabLocation(widget.TabLocationLeading)
 
-	return container
+	scroll := widget.NewScrollContainer(activityTabs)
+	scroll.Resize(fyne.NewSize(200, 200))
+
+	return scroll
 }
 
 // WidgetScreen shows a panel containing widget demos
